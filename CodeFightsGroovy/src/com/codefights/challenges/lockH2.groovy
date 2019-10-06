ca91a005e6be4429febd1c8a3040e22dc767bbef
mrondon8 <mrondon008@gmail.com>
2019-10-05 23:53:25 -0400

    new challenges and interview questions

--- CodeFightsGroovy/src/com/codefights/challenges/lockH2.groovy
+++ CodeFightsGroovy/src/com/codefights/challenges/lockH2.groovy
@@ -1,35 +1,55 @@
 package com.codefights.challenges
-int n = 4
-println n
-def lock = new int[n][n]
 
-def e = [*1 .. n*n]
-//def p = e.permutations()
+/* @kov bechmark (on local env) for n's:
+ * n = 1 => 1
+ * n = 2 => 0
+ * n = 3 => 72
+ * 72
+[1, 8, 9, 2, 4, 3, 6]
+[1, 8, 9, 2, 4, 6, 3]
+[3, 6, 4, 2, 9, 1, 8]
+[3, 6, 4, 2, 9, 8, 1]
+[6, 3, 4, 2, 9, 1, 8]
+[6, 3, 4, 2, 9, 8, 1]
+[8, 1, 9, 2, 4, 3, 6]
+[8, 1, 9, 2, 4, 6, 3]
 
-//println e
-def out = []
-//for(r in p){
-r = e
-for(int z = 0; z < 100_000_000; z++){
-	Collections.shuffle(r)
-	//populate lock
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < n; j++)
-			lock[i][j] = r[i*n + j]
+ * n = 4 => 360
+ * n = 5 => 4320
+ * n = 6 => 60480; 95ms
+ * n = 7 => 1209600; 3471ms
+ * n = 8 => 25945920; 464.775s
+ * n = 9 => 518918400: 152.837min (but it's maybe not the lowest value. I'm used 1..64 range of numbers).
+ */
 
-	//lock.each{println it}
-	//check lock
-	//horizontal
-	def v1 = lock.collect{it[0]}.inject{a,b -> a*b}
-	def v2 = lock.collect{it[-1]}.inject{a,b -> a*b}
+def n = 4
+def r = 1..n*n
+
+List stack = []
+r.each{stack << [it]}
+
+def map = [:].withDefault{[]}
+
+for(; !stack.isEmpty(); ){
 	
-	if(v1 == v2){
-		if(lock.find{it.inject{a,b -> a*b} == v1}){
-			println(lock)
-			println([v1,v2])
-			out << [lock,v1]
+	def current = stack.pop()
+	
+	if(current.size() < 3*n-2){
+		r.each{
+			if( !(it in current) )
+				stack << current + it
 		}
 	}
+	else{
+		def a = current[0..n-1].inject(1){x,y->x*y}
+		def b = current[n-1..2*n-1].inject(1){x,y->x*y}
+		def c = current[2*n-1..-1].inject(1){x,y->x*y}
+		if(a==b && b==c)
+			map[a] << current
+	}
 }
-println "Min $n"
-println (out.min{it[0]})
+
+map.each{k,v-> 
+	println k
+	v.each{println it}
+}
